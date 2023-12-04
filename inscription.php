0c4e911023955b68d3652a22942976b38470475a
calvyngit <109304051+calvyngit@users.noreply.github.com>
2023-12-04 10:39:33 +0100

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -31,50 +31,6 @@
 
     <h2>Utilisateurs inscrits</h2>
     <ul id="userList"></ul>
-
-    <script>
-        let users = [];
-
-        function registerUser() {
-            const regUsername = document.getElementById('regUsername').value;
-            const regPassword = document.getElementById('regPassword').value;
-
-            // Valider les données (ajoutez des vérifications supplémentaires selon vos besoins)
-
-            // Ajouter l'utilisateur au tableau
-            users.push({ username: regUsername, password: regPassword });
-
-            // Afficher la liste des utilisateurs inscrits
-            displayUsers();
-            alert('Inscription réussie !');
-        }
-
-        function loginUser() {
-            const loginUsername = document.getElementById('loginUsername').value;
-            const loginPassword = document.getElementById('loginPassword').value;
-
-            // Vérifier les informations de connexion
-            const loggedIn = users.some(user => user.username === loginUsername && user.password === loginPassword);
-
-            // Afficher le message de bienvenue ou d'échec de connexion
-            if (loggedIn) {
-                alert('Bienvenue, ' + loginUsername + ' !');
-            } else {
-                alert('Échec de la connexion. Veuillez vérifier vos identifiants.');
-            }
-        }
-
-        function displayUsers() {
-            const userListElement = document.getElementById('userList');
-            userListElement.innerHTML = '';
-
-            // Afficher la liste des utilisateurs inscrits
-            users.forEach(user => {
-                const listItem = document.createElement('li');
-                listItem.textContent = user.username;
-                userListElement.appendChild(listItem);
-            });
-        }
-    </script>
+    <script type="text/javascript" src="JS.js"></script>
 </body>
 </html>
